b6be02bf4262dbdbd618fceb5c250d25bd4855ad
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2024-04-06 18:16:07 +0530

    fix es lint issue

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-type Props = {};
-
-export default function ({}: Props) {
+export default function Logo() {
   return (
     <div className="h-10 w-10 rounded-full border-2 border-[#d175b6] bg-gradient-to-t from-[#d175b6] to-yellow-200"></div>
   );
